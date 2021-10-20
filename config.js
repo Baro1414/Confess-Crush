6e35e6c179e15bedd427b689383a536806fe118c
Baro1414 <92845430+Baro1414@users.noreply.github.com>
2021-10-20 13:37:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,18 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Gửi cho ...",
+    introTitle: 'Vũ Lê Đức Hiếu',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    Mà sao bạn ngu Anh vậy?`,
+    btnIntro: '^^Há há há há há^^',
+    title: 'Phải chăng bạn đã thiểu năng từ buổi học đầu tiên 🥰',
+    desc: 'Phải chăng bạn đã bị mất gốc Anh? ',
+    btnYes: 'Mình mất gốc đó thì sao :333 =)))',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao một mình bạn ngu Anh há há',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    reply: 'Không học Anh thì ngu Anh còn học Anh thì vẫn ngu nha b <33333333',
+    mess: 'Mình biết mà 🥰. Mình ngu Anh từ buổi học đầu tiên rùi heheheh',
+    messDesc: 'Học Anh đi b ơi',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
